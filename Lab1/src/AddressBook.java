ebf0c373bb0544e20f94c9c4752a7d2ba8e94637
Amilesh <amilesh2003@gmail.com>
2023-09-16 20:44:29 -0400

    Made changes to main of Address Book

--- Lab1/src/AddressBook.java
+++ Lab1/src/AddressBook.java
@@ -12,6 +12,9 @@ public class AddressBook {
     }
 
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom","Carleton","613-123-4567");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
