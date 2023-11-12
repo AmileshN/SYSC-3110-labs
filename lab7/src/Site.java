e67bfec8af43dae6efb5413ffd7cc7c8ffbcc309
Amilesh <amilesh2003@gmail.com>
2023-11-11 23:20:36 -0500

    Added abstract methods in Site.java

--- lab7/src/Site.java
+++ lab7/src/Site.java
@@ -1,3 +1,13 @@
-public class Site {
+public abstract class Site {
     protected static final double TAX_RATE = 3;
+
+    public double getBillableAmount(){
+        return getBaseAmount() + getTaxAmount();
+    }
+    public abstract double getBaseAmount();
+
+    public abstract double getTaxAmount();
+
+
+
 }
